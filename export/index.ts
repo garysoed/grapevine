3ed08960d36af0b5c9291992cc6c2993f59400f8
Gary Soedarsono <garysoed@gmail.com>
2021-03-28 18:13:44 -0700

    Deleted subjectSource

--- export/index.ts
+++ export/index.ts
@@ -1,3 +1,2 @@
 export {Source, source} from '../src/core/source';
-export {SubjectSource, subjectSource} from '../src/core/subject-source';
 export {Config, Override, Vine} from '../src/core/vine';
