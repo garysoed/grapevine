f4f905ebdf24f2bc7263ddd346b1c5fe74ec60a4
Gary Soedarsono <garysoed@gmail.com>
2021-07-06 13:51:06 -0700

    Added more functions for StateService2

--- export/index.ts
+++ export/index.ts
@@ -1,3 +1,4 @@
 export {Source, source} from '../src/core/source';
 export {Config, Override, Vine} from '../src/core/vine';
-export {$stateService, $resolveState, $resolveStateOp, $modifyState, $modifyStateOp} from '../src/core/state';
+export {mutablePathSource, $stateService, $stateService2, $resolveState, $resolveStateOp, $modifyState, $modifyStateOp} from '../src/core/state';
+export {Id} from '../src/core/id';
--- src/core/state.ts
+++ src/core/state.ts
@@ -1,6 +1,8 @@
-import {StateService} from 'gs-tools/export/state';
+import {ObjectPath, PathProvider, RootStateId, StateService, StateService2} from 'gs-tools/export/state';
 
-import {source} from './source';
+import {MutableState} from '../../../tools/src/state/state-service-2';
+
+import {Source, source} from './source';
 
 export const $stateService = source('stateService', () => new StateService());
 
@@ -20,3 +22,23 @@ export const $modifyStateOp = source('modifyStateOp', vine => {
   const stateService = $stateService.get(vine);
   return stateService.modifyOperator.bind(stateService);
 });
+
+export const $stateService2 = source('stateService2', () => new StateService2());
+
+export function mutablePathSource<T>(label: string, rootStateId: Source<RootStateId<T>>): Source<ObjectPath<T>>;
+export function mutablePathSource<R, T>(label: string, rootStateId: Source<RootStateId<R>>, provider: PathProvider<R, T>): Source<ObjectPath<T>>;
+export function mutablePathSource(
+    label: string,
+    rootStateId: Source<RootStateId<unknown>>,
+    provider?: PathProvider<unknown, unknown>,
+): Source<ObjectPath<unknown>> {
+  return source(label, vine => {
+    const service = $stateService2.get(vine);
+
+    if (provider) {
+      return service.mutablePath(rootStateId.get(vine), provider);
+    } else {
+      return service.mutablePath(rootStateId.get(vine) as RootStateId<MutableState<unknown>>);
+    }
+  });
+}
