20d5d63c249a59ff1550c2cd67b0202573ca2a8e
Gary Soedarsono <garysoed@gmail.com>
2022-01-09 13:15:07 -0800

    Deleted the state supports

--- export/index.ts
+++ export/index.ts
@@ -1,4 +1,4 @@
 export {Source, source} from '../src/core/source';
 export {Config, Override, Vine} from '../src/core/vine';
-export {immutablePathSource, mutablePathSource, $stateService, rootStateIdSource} from '../src/core/state';
+export {$stateService} from '../src/core/state';
 export {Id} from '../src/core/id';
--- src/core/state.ts
+++ src/core/state.ts
@@ -1,46 +1,6 @@
-import {MutableState, ObjectPath, PathProvider, RootStateId, StateService} from 'gs-tools/export/state';
+import {StateService} from 'gs-tools/export/state';
 
-import {Source, source} from './source';
-import {Vine} from './vine';
+import {source} from './source';
 
 
 export const $stateService = source(() => new StateService());
-
-export function rootStateIdSource<T>(provider: (vine: Vine) => T): Source<RootStateId<T>> {
-  return source(vine => $stateService.get(vine).addRoot(provider(vine)));
-}
-
-export function immutablePathSource<T>(rootStateId: Source<RootStateId<T>>): Source<ObjectPath<T>>;
-export function immutablePathSource<R, T>(rootStateId: Source<RootStateId<R>>, provider: PathProvider<R, T>): Source<ObjectPath<T>>;
-export function immutablePathSource(
-    rootStateId: Source<RootStateId<unknown>>,
-    provider?: PathProvider<unknown, unknown>,
-): Source<ObjectPath<unknown>> {
-  return source(vine => {
-    const service = $stateService.get(vine);
-
-    if (provider) {
-      return service.immutablePath(rootStateId.get(vine), provider);
-    } else {
-      return service.immutablePath(rootStateId.get(vine));
-    }
-  });
-}
-
-
-export function mutablePathSource<T>(rootStateId: Source<RootStateId<MutableState<T>>>): Source<ObjectPath<MutableState<T>>>;
-export function mutablePathSource<R, T>(rootStateId: Source<RootStateId<R>>, provider: PathProvider<R, MutableState<T>>): Source<ObjectPath<MutableState<T>>>;
-export function mutablePathSource(
-    rootStateId: Source<RootStateId<unknown>|RootStateId<MutableState<unknown>>>,
-    provider?: PathProvider<unknown, MutableState<unknown>>,
-): Source<ObjectPath<unknown>> {
-  return source(vine => {
-    const service = $stateService.get(vine);
-
-    if (provider) {
-      return service.mutablePath(rootStateId.get(vine), provider);
-    } else {
-      return service.mutablePath((rootStateId as Source<RootStateId<MutableState<unknown>>>).get(vine));
-    }
-  });
-}
