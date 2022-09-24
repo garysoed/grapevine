8960cf20c60009c22661dbb47ee9a3ea93ed4ca2
Gary Soedarsono <garysoed@gmail.com>
2022-09-24 11:03:53 -0700

    Deleted state

--- export/index.ts
+++ export/index.ts
@@ -1,4 +1,3 @@
 export {Source, source} from '../src/core/source';
 export {Config, Override, Vine} from '../src/core/vine';
-export {$stateService} from '../src/core/state';
 export {Id} from '../src/core/id';
--- src/core/state.ts
+++
@@ -1,6 +0,0 @@
-import {StateService} from 'gs-tools/export/state';
-
-import {source} from './source';
-
-
-export const $stateService = source(() => new StateService());
