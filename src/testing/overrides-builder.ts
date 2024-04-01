9d5063effe289c4b7a8bf4c0d41c4d8dfe3910f6
Gary Soedarsono <garysoed@garys-mbp.home>
2024-03-31 21:19:13 -0400

    Updated overrides to use cached values

--- src/testing/overrides-builder.ts
+++ src/testing/overrides-builder.ts
@@ -1,12 +1,18 @@
+import {CachedValue} from 'gs-tools/export/data';
+
 import {Id} from '../core/id';
 import {Override, override} from '../core/vine';
 
 export abstract class OverridesBuilder {
-  readonly #overrides = new Map<Id<unknown>, Override<unknown>>(
-    this.defaultOverrideIds.map((id) => {
-      return [id, override(id, this.getOverride(id))];
-    }),
-  );
+  readonly overridesCache = new CachedValue<
+    Map<Id<unknown>, Override<unknown>>
+  >(() => {
+    return new Map<Id<unknown>, Override<unknown>>(
+      this.defaultOverrideIds.map((id) => {
+        return [id, override(id, this.getOverride(id))];
+      }),
+    );
+  });
 
   constructor(
     private readonly defaultOverrideIds: ReadonlyArray<Id<unknown>>,
@@ -16,11 +22,11 @@ export abstract class OverridesBuilder {
 
   protected addOverride<T>(id: Id<T>): T {
     const overrideInstance = this.getOverride(id);
-    this.#overrides.set(id, override(id, overrideInstance));
+    this.overridesCache.value.set(id, override(id, overrideInstance));
     return overrideInstance as T;
   }
 
   get overrides(): ReadonlyArray<Override<unknown>> {
-    return [...this.#overrides.values()];
+    return [...this.overridesCache.value.values()];
   }
 }
